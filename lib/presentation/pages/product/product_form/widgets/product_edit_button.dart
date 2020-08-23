09966d582583bd59a34962329e71d92bd014e367
Leandro Pinto <leandro.pnto@gmail.com>
2020-08-23 15:17:51 -0300

    Botao de editar produto.

--- lib/presentation/pages/product/product_form/widgets/product_edit_button.dart
+++ lib/presentation/pages/product/product_form/widgets/product_edit_button.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:superloja/application/auth/auth_bloc.dart';
+import 'package:superloja/application/product/product_form/product_form_bloc.dart';
 
 class ProductEditButton extends StatelessWidget {
+  final AuthBloc bloc = AuthBloc.to;
+  final ProductFormBloc productBloc = ProductFormBloc.to;
+
   @override
   Widget build(BuildContext context) {
-//    return BlocBuilder<AuthBloc, AuthState>(
-//        builder: (context, state) => state.maybeMap(
-//            authenticated: (e) => e.user.isAdmin
-//                ? IconButton(
-//                    icon: const Icon(Icons.edit),
-//                    onPressed: () => Navigator.of(context).pushNamed(
-//                      "/product/edit",
-//                      arguments: context.bloc<ProductFormBloc>().state.product,
-//                    ),
-//                  )
-//                : Container(),
-//            orElse: () => Container()));
-
-    return Container();
+    return Obx(
+      () => bloc.admin.value
+          ? IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {
+                Get.put(ProductFormBloc(Get.find()));
+                Get.toNamed("/product/edit");
+                productBloc.load(productBloc.product.value.id.getOrCrash());
+              })
+          : Container(),
+    );
   }
 }
