7ff58f735eac7b3092bbb4b2f17b4758593bb01f
Leandro Pinto <leandro.pnto@gmail.com>
2020-08-26 11:41:11 -0300

    Ajuste do bloc

--- lib/application/address/address_bloc.dart
+++ lib/application/address/address_bloc.dart
@@ -2,14 +2,17 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:meta/meta.dart';
+import 'package:superloja/application/cart/cart_bloc.dart';
 import 'package:superloja/domain/address/address.dart';
 import 'package:superloja/domain/cep/i_cep_service.dart';
 import 'package:superloja/domain/cep/value_objects.dart';
 import 'package:superloja/domain/core/failures.dart';
 import 'package:superloja/domain/distance/i_shippment_service.dart';
+import 'package:superloja/injection.dart';
 
 part 'address_bloc.freezed.dart';
 part 'address_event.dart';
@@ -59,15 +62,27 @@ class AddressBloc extends Bloc<AddressEvent, AddressState> {
               () => {}, //se vazio nao faz nada
               (a) => {
                     a.fold(
-                      (l) => {print("Erro $l")}, // se deu erro
+                      (l) => {debugPrint("Erro $l")}, // se deu erro
                       (address) {
                         _shippmentService
                             .totalCust(address.coordenates.longitude,
                                 address.coordenates.latitude)
                             .then(
                               (value) => value.fold(
-                                (l) => print("Erro $l"),
-                                (r) => print("Total: $r"),
+                                (l) => debugPrint("Erro $l"),
+                                (r) {
+                                  getIt<CartBloc>().add(
+                                    CartEvent.setAddress(
+                                      address.copyWith(
+                                        number: state.number.getOrCrash(),
+                                        complement:
+                                            state.complement.getOrCrash(),
+                                      ),
+                                    ),
+                                  );
+                                  debugPrint(
+                                      "$r - ${state.number} - ${state.complement}");
+                                },
                               ),
                             );
                       },
--- lib/domain/address/address.dart
+++ lib/domain/address/address.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:superloja/domain/address/state.dart';
 import 'package:superloja/domain/cep/address_coordenates.dart';
 
@@ -11,13 +12,56 @@ class Address {
   final String complement;
   final String zipcode;
   final String street;
+  final String number;
 
-  Address(
-      {this.city,
-      this.state,
-      this.coordenates,
-      this.districty,
-      this.complement,
-      this.zipcode,
-      this.street});
+  const Address({
+    @required this.city,
+    @required this.state,
+    @required this.coordenates,
+    @required this.districty,
+    @required this.complement,
+    @required this.zipcode,
+    @required this.street,
+    @required this.number,
+  });
+
+  Address copyWith({
+    City city,
+    State state,
+    AddressCoordenates coordenates,
+    String districty,
+    String complement,
+    String zipcode,
+    String street,
+    String number,
+  }) {
+    if ((city == null || identical(city, this.city)) &&
+        (state == null || identical(state, this.state)) &&
+        (coordenates == null || identical(coordenates, this.coordenates)) &&
+        (districty == null || identical(districty, this.districty)) &&
+        (complement == null || identical(complement, this.complement)) &&
+        (zipcode == null || identical(zipcode, this.zipcode)) &&
+        (street == null || identical(street, this.street)) &&
+        (number == null || identical(number, this.number))) {
+      return this;
+    }
+
+    return Address(
+      city: city ?? this.city,
+      state: state ?? this.state,
+      coordenates: coordenates ?? this.coordenates,
+      districty: districty ?? this.districty,
+      complement: complement ?? this.complement,
+      zipcode: zipcode ?? this.zipcode,
+      street: street ?? this.street,
+      number: number ?? this.number,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Address{city: $city, state: $state, coordenates: $coordenates,'
+        ' districty: $districty, complement: $complement, zipcode: $zipcode, '
+        'street: $street, number: $number}';
+  }
 }
